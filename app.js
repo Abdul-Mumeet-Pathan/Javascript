4742958cc33bbb5156f9ea98c47a7325c1ffdba6
Kafka05 <abdulmumeet35@gmail.com>
2024-10-18 12:24:56 +0600

    variable, comment , statement.

---
+++ app.js
@@ -0,0 +1,30 @@
+// Variable 
+
+let firstname='kafka';
+console.log(firstname);
+
+let f ='Mango';
+console.log(`${f} is my dash`);
+
+var a=20;  //alternative of let but its not good because 
+var a=20; // you can redeclare. It is a global variable.
+
+// you can also do this 
+console.log(coun);
+var coun=50;
+//var is hosted
+
+
+// but in let you can not redeclare but reassign
+// block scooped or local 
+let b=10; 
+b=11;
+
+
+
+
+// To print 
+console.log('Hello');
+
+//Comment 
+/* Multi Line Comment */
